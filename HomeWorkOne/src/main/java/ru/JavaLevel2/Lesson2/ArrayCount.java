7fb201b90632fcf1911546249b0e941ca47f5c12
andreyznsk <andreyznsk@gmail.com>
2020-11-30 11:50:02 +0700

    Доделал в соответствии с заданием

--- HomeWorkOne/src/main/java/ru/JavaLevel2/Lesson2/ArrayCount.java
+++ HomeWorkOne/src/main/java/ru/JavaLevel2/Lesson2/ArrayCount.java
@@ -1,5 +1,6 @@
 package ru.JavaLevel2.Lesson2;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class ArrayCount {
@@ -7,51 +8,36 @@ public class ArrayCount {
     private static final int SIZE = 4;//Жесткая длинна в соотвествии с заданием
 
    public static int arrCount(String[][] s) throws RuntimeException{ //Метод подсчтеа массива
-    if(s.length!=SIZE) throw new MyArraySizeException(s.length);//выброс исключения по длинне 1 уровня
+       int summ = 0;
+       if(s.length!=SIZE) throw new MyArraySizeException(s.length);//выброс исключения по длинне 1 уровня
     for (String[] strings : s) if (strings.length != SIZE) throw new MyArraySizeException(s.length);//выброс исключения по длинне второго уровня
     for (int i = 0; i < SIZE; i++) //Двойной цикл проверки числовых значений в массиве
         for (int j = 0; j < SIZE; j++)
             try {
-                Integer.parseInt(s[i][j]);
+                summ +=  Integer.parseInt(s[i][j]);
             } catch(NumberFormatException e) {
-                s[i][j] = "0";//Принудительное исправление ошибки
-                System.err.println("Принудительно заменяем элемент: " + i + ',' + j + " на 0");
-                System.err.println("Исключительная сумма без String = "  + arrCount(s));//Рекурсивный вызов метода для подсчеты суммы с ошибкой
                 throw new MyArrayDataException(i,j);//выброс ошибки с информацией по последнему элементу
             }
 
-    int summ = 0;//Стандартная реализация, если ошибок нет
-    for (int i = 0; i < SIZE; i++) {
-        for (int j = 0; j < SIZE; j++) {
-            summ += Integer.parseInt(s[i][j]);
-
-        }
-    }
     return summ;
 }
 
 public static void printStringArray(String[][] s) throws MyArraySizeException {
     if(s.length!=SIZE) throw new MyArraySizeException(s.length);
     for (String[] strings : s) if (strings.length != SIZE) throw new MyArraySizeException(s.length);
+    int arr[] = new int[SIZE];
 
-    System.out.println();
-    System.out.print("  ");
-
-    // Верхняя "Легенда"
-    for (int i = 0; i < SIZE; i++) {
-        System.out.printf("%2d", i);
-    }
-    System.out.println();
-
-    for (int i = 0; i < SIZE; i++) {
-        System.out.printf("%2d ",(i));
-        for (int j = 0; j < SIZE; j++) {
-            System.out.print(s[i][j] + " ");
+    try {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                arr[j] = Integer.parseInt(s[i][j]);
+            }
+            System.out.println(Arrays.toString(arr));
         }
-        System.out.println();
+    } catch (NumberFormatException e) {
+        System.err.println("Массив не числовой");
     }
 
-    // Дополнительный отступ
     System.out.println();
 }
 
@@ -66,11 +52,10 @@ public static void printStringArray(String[][] s) throws MyArraySizeException {
 
         String[][] string = new String[SIZE][SIZE];//Не числовой массив
         for (int i = 0; i < SIZE; i++) {
-            //string[i][j] = Integer.toString(random.nextInt(100));
             Arrays.fill(string[i], "1");
         }
         string[3][1] = "a";
-        string[2][1] = "a";
+        //string[2][1] = "a";
 
         try {
             printStringArray(string);
@@ -81,13 +66,10 @@ public static void printStringArray(String[][] s) throws MyArraySizeException {
             System.err.println(e.getMessage());
         }
         System.out.println("Еще раз Считаем сумму массива без исключений:");
+
+        string[3][1] = "1";
         try {
             printStringArray(string);
-            } catch (MyArraySizeException e) {
-            System.err.println(e.getMessage());
-        }
-
-        try {
              System.out.println("Сумма массива = " + arrCount(string));
         } catch (MyArraySizeException e){
             System.err.println(e.getMessage());
