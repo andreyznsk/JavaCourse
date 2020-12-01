0aa0234f53a1bf94ec3e47b2b1b9b177cf8798fd
andreyznsk <andreyznsk@gmail.com>
2020-12-01 13:13:14 +0700

    new Linux

---
+++ HomeWorkOne/src/main/java/ru/JavaLevel2/Lesson3/Lesson3.java
@@ -0,0 +1,20 @@
+package ru.JavaLevel2.Lesson3;
+
+import java.util.ArrayList;
+
+public class Lesson3 {
+    public static void main(String[] args) {
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("A");
+        arrayList.add("B");
+        arrayList.add("C");
+        arrayList.add("D");
+        arrayList.add("E");
+        arrayList.add(1, "А0");
+        System.out.println(arrayList);
+        arrayList.remove("E");
+        arrayList.remove(2);
+        System.out.println(arrayList);
+
+    }
+}
