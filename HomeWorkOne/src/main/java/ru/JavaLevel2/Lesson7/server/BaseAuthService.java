71873dbaad963787241a5445b26eda0c52c8cbf3
andreyznsk <andreyznsk@gmail.com>
2020-12-28 16:33:11 +0700

    Выполнил 1ую часть

--- HomeWorkOne/src/main/java/ru/JavaLevel2/Lesson7/server/BaseAuthService.java
+++ HomeWorkOne/src/main/java/ru/JavaLevel2/Lesson7/server/BaseAuthService.java
@@ -1,10 +1,38 @@
 package ru.JavaLevel2.Lesson7.server;
 
-import java.util.HashMap;
-import java.util.Map;
-    public class BaseAuthService implements AuthService {
+import java.sql.*;
+public class BaseAuthService implements AuthService {
 
-        private static Map<User, String> USERS = null; /*= Map.of(
+        private static Connection connection;
+        private static Statement stmt;
+        private static PreparedStatement psSelect;
+
+        private static void prepareAllStatements() throws SQLException {
+            psSelect = connection.prepareStatement("SELECT nickname FROM users WHERE login = ? AND password = ?;");
+        }
+
+        private static void connect() throws Exception {
+            Class.forName("org.sqlite.JDBC");
+            connection = DriverManager.getConnection("jdbc:sqlite:main.db");
+            stmt = connection.createStatement();
+        }
+
+        private static void disconnect() {
+            try {
+                stmt.close();
+                System.out.println("Data base has been closed");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+            /*private static Map<User, String> USERS = null; = Map.of(
                 new User("login1", "pass1", "Peter"), "Peter",
                 new User("login2", "pass2", "Alexey"), "Alexey",
                 new User("login3", "pass3", "Oleg"), "Oleg"
@@ -14,11 +42,22 @@ import java.util.Map;
         @Override
         public void start() {
             System.out.println("Auth service is running");
-            USERS = new HashMap<>();
+           /* USERS = new HashMap<>();
             USERS.put(new User("2","2","Perter"), "Peter");
             USERS.put(new User("3","3","Alexey"), "Alexey");
             USERS.put(new User("4","4","Oleg"), "Oleg");
             USERS.put(new User("1","1","Andreyz"), "Andreyz");
+*/
+            try {
+                connect();
+                prepareAllStatements();
+                System.out.println("Connect to bd main is successful");
+            } catch (Exception e) {
+                System.err.println(e);
+            } finally {
+                //disconnect();
+            }
+
 
 
 
@@ -27,12 +66,27 @@ import java.util.Map;
         @Override
         public void stop() {
             System.out.println("Auth service has been stopped");
+            disconnect();
         }
 
         @Override
-        public String getNickByLoginPass(String login, String password) {
-            User requestedUser = new User(login, password, null);
-            return USERS.get(requestedUser);
+        public String getNickByLoginPass(String login, String password) {//Изменил на метод идентификации из БД.
+            /*User requestedUser = new User(login, password, null);
+            return USERS.get(requestedUser);*/
+            try {//Блок провеки через подготовленный запрос
+                psSelect.setString(1,login);
+                psSelect.setString(2,password);
+                ResultSet rs = psSelect.executeQuery();
+                while (rs.next()){
+                    System.out.println(rs.getString("nickname"));
+                    return rs.getString("nickname");
+                }
+
+            } catch (SQLException throwables) {
+                System.err.println(throwables);
+                throwables.printStackTrace();
+            }
+            return null;
         }
     }
 
